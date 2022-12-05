c23b17546aced05f5ff5dd291ef64b364ad069cc
chevonnelise <elise.chevonne@gmail.com>
2022-12-05 12:16:21 +0000

    set up add

--- lab05/index.js
+++ lab05/index.js
@@ -1,42 +1,31 @@
-// load in the Express package
-// aka includes the Express package
-const express = require("express");
+// 1. SETUP EXPRESS
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
 
-// create an express application
-// the `express` function will create a new express application
-// and we store the application in the `app` variable
+// 1a. create the app
 const app = express();
 
-// to set up a route
-// a route is a pairing of an URL to a function
-app.get("/", function(request, response){
-    response.send("<h1>Hello World</h1>");
+// 1b setup our view engine (aka template engine)
+// tell Express that we are using hbs
+app.set('view engine', 'hbs');
+
+// 1c setup wax-on
+wax.on(hbs.handlebars);
+
+// 1d tell wax-on where to find the layout files
+wax.setLayoutPath("./views/layouts");
+
+// 1e use static files (i.e, images, css, js etc. -- that is, all content not generated
+// by our routes)
+app.use(express.static('public'));
+
+// 2. CREATE ROUTES
+app.get('/', function(req,res){
+    res.send("Hello world");
 })
 
-app.get('/about-us', function(request, response){
-    // the annoymous function will have two parameters
-    // the first one is the request (i.e whatever the client sends to the server)
-    // the second one is the response -- what the server will send back to the client
-    response.send("<h1>About Us</h1>");
-})
-
-// one way for the server to recieve data from the browser
-// is via route parameters
-// a route parameter is like a placeholder in the URL
-app.get("/hello/:name", function(req,res){
-    const name = req.params.name;
-    res.send("Hello there " + name)
-})
-
-app.get('/square/:number', function(req,res){
-    const number = req.params.number;
-    const squared = parseInt(number) ** 2;
-    res.send("Squared is " + squared);
-})
-
-// start the server at port 3000
-// the second parameter is a function that is called
-// when the server starts
+// 3. START SERVER (No routers after you've started server)
 app.listen(3000, function(){
-    console.log("Hello world");
+    console.log("Server has started");
 })
